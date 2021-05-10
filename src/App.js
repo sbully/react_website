719cc6e8e10e4c7974bfa2c263b276d18d0eb52c
sbully <bully.sebastien39@gmail.com>
2021-05-10 17:56:44 +0200

    add navbar and background video

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import Navbar from './components/navbar/NavBar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import Home from './page/Home';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>hey</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/page/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+import HeroSection from '../components/herosection/HeroSection';
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
+export default Home;
